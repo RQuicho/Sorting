212606e5df6a327fdca708afbec1326499b786b7
RQuicho <raymondquicho@gmail.com>
2023-11-27 23:02:55 -0800

    adding merge and mergeSort functions

--- bubble.js
+++ bubble.js
@@ -1,5 +1,7 @@
 function bubbleSort(arr) {
+  // going through each element in the array
   for (let i=0; i<arr.length; i++) {
+    // going through each element in the shortened array (largest number of previous iteration already at end of array)
     for (let j=0; j<arr.length-i; j++) {
       if (arr[j] > arr[j+1]) {
         let temp = arr[j];
--- merge.js
+++ merge.js
@@ -1,5 +1,37 @@
-function merge() {}
+function merge(arr1, arr2) {
+  const results = [];
+  let i = 0;
+  let j = 0;
 
-function mergeSort() {}
+  while (i<arr1.length && j<arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      results.push(arr1[i]);
+      i++;
+    } else {
+      results.push(arr2[j]);
+      j++;
+    }
+  }
+  while (i<arr1.length) {
+    results.push(arr1[i]);
+    i++;
+  }
+  while (j<arr2.length) {
+    results.push(arr2[j]);
+    j++;
+  }
+  return results;
+}
+
+function mergeSort(arr) {
+  // base case
+  if (arr.length <= 1) return arr;
+
+  // typcial case
+  const mid = Math.floor(arr.length/2); // split array into two
+  const left = mergeSort(arr.slice(0, mid)); // beg to middle of array
+  const right = mergeSort(arr.slice(mid)); // middle to end of array
+  return merge(left, right);
+}
 
 module.exports = { merge, mergeSort};
